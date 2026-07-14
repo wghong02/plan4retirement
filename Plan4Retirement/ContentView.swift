0d16de924b9263aa5c7c6c4fc5281c9550da92d7
Gilbert <wghong02@gmail.com>
2026-07-14 00:01:03 -0700

    Initial Commit

---
+++ Plan4Retirement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plan4Retirement
+//
+//  Created by Gilbert Hong on 7/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plan4Retirement/Plan4RetirementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plan4RetirementApp.swift
+//  Plan4Retirement
+//
+//  Created by Gilbert Hong on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Plan4RetirementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
